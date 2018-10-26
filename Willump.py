d3ef5b7e9794f54eb0d8623053db7ef4afb9d421
Omar <omarg96@hotmail.co.uk>
2018-10-26 19:37:09 +0100

    added first champion info function

--- Willump.py
+++ Willump.py
@@ -2,15 +2,32 @@ from DevKey import *
 import json
 import urllib.request
 APIKey = key
-print(APIKey)
-SumName = input("Enter Summoner Name: ")
+summonerID='' 
 
-NSN = SumName.replace(' ','%20')
-url="https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"
-url +=NSN
-url += "?api_key=RGAPI-38dcd00d-16b6-4912-b44d-97941d03b644"
-print(url)
-contents = urllib.request.urlopen(url).read()
-infoList = (json.loads(contents))
+def SumDetails():#function that retrieves summoner information from summoner name (EUW only for the moment)
+    global summonerID
+    SumName = input("Enter Summoner Name: ")
+    NSN = SumName.replace(' ','%20')
+    url="https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"
+    url +=NSN
+    url += "?api_key="
+    url += APIKey
+    contents = urllib.request.urlopen(url).read()
+    infoList = (json.loads(contents))
+    summonerID = str(infoList['id'])#stores summoner id for use with other api functions
+    return infoList
 
-print(infoList)
+def ChampDetails(): #function that returns information related to 
+    if summonerID == '':
+        SumDetails()
+    else:
+        url='https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'
+        url += summonerID
+        url += '?api_key='
+        url += APIKey
+        contents = urllib.request.urlopen(url).read()
+        ChampDetailsList = (json.loads(contents))
+        return ChampDetailsList
+
+print(SumDetails())
+print(ChampDetails())
