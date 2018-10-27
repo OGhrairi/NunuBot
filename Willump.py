ab0f0cb6b79e643f540185c01795dddff0576b78
Omar <omarg96@hotmail.co.uk>
2018-10-27 19:56:50 +0100

    placed championID finder in dedicated function, added funtion to retrieve champion stats for a given account

--- Willump.py
+++ Willump.py
@@ -3,7 +3,9 @@ import json
 import urllib.request
 import os
 APIKey = key
-summonerID='' 
+summonerID='' #numerical summoner identifier
+champID='' #numerical champion identifier
+SumChampInfo = []
 def pathSel(fileName): #function that converts input filename to full ile path
     #file must be in same path as this file, and takes format 'FileName.FileExt'
     ProjectPath = (os.path.dirname(os.path.realpath(__file__)))
@@ -20,7 +22,8 @@ for n in data["data"]: #goes through each champion, adds the key to one array,
     CN.append(data["data"][n]['key'])
 champDetails = dict(zip(CK,CN))#combines name and key of each champ into tuple dictionary
 #print(champDetails['aatrox'])
-def SumDetails():#function that retrieves summoner information from summoner name (EUW only for the moment)
+
+def SumInfo():#function that retrieves summoner information from summoner name (EUW only for the moment)
     global summonerID
     SumName = input("Enter Summoner Name: ")
     NSN = SumName.replace(' ','%20')
@@ -33,24 +36,32 @@ def SumDetails():#function that retrieves summoner information from summoner nam
     summonerID = str(infoList['id'])#stores summoner id for use with other api functions
     return infoList
 
-def ChampDetails(): #function that returns information related to a selected champion
+def ChampNumber():#function that finds numerical id of a champ and stores in global champID var
+    global champID
+    champName = input("Enter Champion Name (lower case no spaces) ") #take champ name input to find id
+    if champName in champDetails: #check validity of champ name
+        champID = (champDetails.get(champName)) #if name is vaild, retrieve champ id
+        return
+        #print(champID) 
+    else:    
+        return("champ does not exist") #inform if champ name isn't valid
+def ChampSumInfo(): #function that returns information related to a selected champion
+    global SumChampInfo
     if summonerID == '':
-        SumDetails()
+        SumInfo()
+        ChampSumInfo()
     else:
-        champName = input("Enter Champion Name (no spaces)") #take champ name input to find id
-        if champName in champDetails: #check validity of champ name
-            champID = (champDetails.get(champName)) #if name is vaild, retrieve champ id
-            #print(champID) 
-        else:    
-            return("champ does not exist") #inform if champ name isn't valid
         url='https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'
         url += summonerID
+        url += '/by-champion/'
+        url += champID
         url += '?api_key='
         url += APIKey
         contents = urllib.request.urlopen(url).read()
-        ChampDetailsList = (json.loads(contents))
+        SumChampInfo = (json.loads(contents))
+        #above value has keys: playerId, championId, championLevel, championPoints, lastPlayTime
+        #championPointsSinceLastLevel, championPointsUntilNextLevel, chestGranted, tokensEarned
+        return
 
-        return ChampDetailsList
-
-print(SumDetails())
-print(ChampDetails())
+def champStats(): #return champion information from json file 
+  #  if champID = ''
