7b69968abdd9e1d9fbd96a02fdb53215777c5723
Omar <omarg96@hotmail.co.uk>
2018-10-27 12:35:46 +0100

    Added ability to retrieve champ id from entered champ name

--- Willump.py
+++ Willump.py
@@ -1,4 +1,4 @@
-from DevKey import *
+from DevKey import * #apikey must be stored in a DevKey.py file as variable 'key'
 import json
 import urllib.request
 import os
@@ -16,10 +16,10 @@ CK=[]
 CN=[]
 for n in data["data"]: #goes through each champion, adds the key to one array,
     #and the id to another, to be put together in a dictionary
-    CK.append(data["data"][n]['key'])
-    CN.append(data["data"][n]['id'])
+    CK.append(data["data"][n]['id'].lower())
+    CN.append(data["data"][n]['key'])
 champDetails = dict(zip(CK,CN))#combines name and key of each champ into tuple dictionary
-#print(champDetails['266'])
+#print(champDetails['aatrox'])
 def SumDetails():#function that retrieves summoner information from summoner name (EUW only for the moment)
     global summonerID
     SumName = input("Enter Summoner Name: ")
@@ -37,13 +37,19 @@ def ChampDetails(): #function that returns information related to a selected cha
     if summonerID == '':
         SumDetails()
     else:
+        champName = input("Enter Champion Name (no spaces)") #take champ name input to find id
+        if champName in champDetails: #check validity of champ name
+            champID = (champDetails.get(champName)) #if name is vaild, retrieve champ id
+            #print(champID) 
+        else:    
+            return("champ does not exist") #inform if champ name isn't valid
         url='https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'
         url += summonerID
         url += '?api_key='
         url += APIKey
         contents = urllib.request.urlopen(url).read()
         ChampDetailsList = (json.loads(contents))
-        
+
         return ChampDetailsList
 
 print(SumDetails())
